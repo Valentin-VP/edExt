73da29d802f56c432b63cc8509a88f48a3c3e3e3
Mauricio <mauricio_iglesias@outlook.es>
2020-09-08 12:39:38 -0300

    DtInstituto

---
+++ Proyecto/src/main/java/datatypes/DtInstituto.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class DtInstituto {
+	private String nombre;
+	
+	public DtInstituto(String nombre) {
+		super();
+		this.nombre=nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
