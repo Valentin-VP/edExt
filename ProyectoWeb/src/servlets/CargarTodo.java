37f3a92fdf02f43fb924129003cd10e3b97b5eb8
rcastro33 <rcastro@programmer.net>
2020-11-14 19:03:28 -0300

    CargarTodo en version Publish

--- ProyectoWeb/src/servlets/CargarTodo.java
+++ ProyectoWeb/src/servlets/CargarTodo.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -10,12 +11,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.rpc.ServiceException;
 
-import datatypes.DtInstituto;
-import excepciones.SinCategorias;
-import excepciones.SinInstitutos;
-import interfaces.Fabrica;
-import interfaces.IControladorConsultaCurso;
+import publicadores.DtInstituto;
+import publicadores.SinCategorias;
+import publicadores.SinInstitutos;
+import publicadores.ControladorAltaCursoPublish;
+import publicadores.ControladorAltaCursoPublishService;
+import publicadores.ControladorAltaCursoPublishServiceLocator;
+import publicadores.ControladorConsultaCursoPublish;
+import publicadores.ControladorConsultaCursoPublishService;
+import publicadores.ControladorConsultaCursoPublishServiceLocator;
 
 /**
  * Servlet implementation class CargarTodo
@@ -23,7 +29,9 @@ import interfaces.IControladorConsultaCurso;
 @WebServlet("/CargarTodo")
 public class CargarTodo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	private String remoteerror = new String();
+	
     public CargarTodo() {
         super();
     }
@@ -33,8 +41,8 @@ public class CargarTodo extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Fabrica fabrica = Fabrica.getInstancia();
-		IControladorConsultaCurso icon = fabrica.getIControladorConsultaCurso();
+		//Fabrica fabrica = Fabrica.getInstancia();
+		//IControladorConsultaCurso icon = fabrica.getIControladorConsultaCurso();
 		HttpSession sesion = request.getSession(true);
 		RequestDispatcher rd;
 		
@@ -43,20 +51,20 @@ public class CargarTodo extends HttpServlet {
 		ArrayList <String> institutosPlataforma = new ArrayList <String>();
 		
 		try {
-			categoriasPlataforma = icon.listarCategorias();
+			categoriasPlataforma = listarCategorias();
 			sesion.setAttribute("categoriasPlataforma", categoriasPlataforma);
-		} catch (SinCategorias e) {
+		} catch (SinCategorias | ServiceException e) {
 			categoriasPlataforma.add("No se encontraron categorias");
 			sesion.setAttribute("categoriasPlataforma", categoriasPlataforma);
 		}
 		
 		try {
-			dtinstitutosPlataforma = icon.listarInstitutos();
+			dtinstitutosPlataforma = listarInstitutos();
 			for(DtInstituto dtin: dtinstitutosPlataforma) {
 				institutosPlataforma.add(dtin.getNombre());
 			}
 			sesion.setAttribute("institutosPlataforma", institutosPlataforma);
-		} catch (SinInstitutos e) {
+		} catch (SinInstitutos | ServiceException e) {
 			institutosPlataforma.add("No se encontraron institutos");
 			sesion.setAttribute("institutosPlataforma", institutosPlataforma);
 		}
@@ -64,5 +72,48 @@ public class CargarTodo extends HttpServlet {
 		rd = request.getRequestDispatcher("/index.jsp");
 		rd.forward(request, response);
 	}
-
+	
+	public ArrayList<String> listarCategorias() throws RemoteException, ServiceException {
+		ControladorConsultaCursoPublishService cps = new ControladorConsultaCursoPublishServiceLocator();
+		ControladorConsultaCursoPublish port = cps.getControladorConsultaCursoPublishPort();
+		String[] categorias;
+		ArrayList<String> retorno = new ArrayList<String>();
+		categorias = port.listarCategorias();
+		if(!port.getMensaje().equals("vacio")){
+			remoteerror = port.getMensaje();
+			port.setMensaje("vacio");
+			System.out.println("Se obtuvo un error o no hay categorias");
+			throw new SinCategorias();
+		}
+		else {
+			for (int i = 0; i < categorias.length; ++i) {
+			    retorno.add(categorias[i]);
+			}
+		}
+		/*
+		 * if (categorias.length == 0) { remoteerror = port.getMensaje(); throw new
+		 * RemoteException(); }
+		 */
+		return retorno;
+	}
+	
+	public ArrayList<DtInstituto> listarInstitutos() throws RemoteException, ServiceException {
+		ControladorConsultaCursoPublishService cps = new ControladorConsultaCursoPublishServiceLocator();
+		ControladorConsultaCursoPublish port = cps.getControladorConsultaCursoPublishPort();
+		DtInstituto[] institutos;
+		ArrayList<DtInstituto> retorno = new ArrayList<DtInstituto>();
+		institutos = port.listarInstitutos();
+		if(!port.getMensaje().equals("vacio")){
+			remoteerror = port.getMensaje();
+			port.setMensaje("vacio");
+			System.out.println("Se obtuvo un error o no hay institutos");
+			throw new SinInstitutos();
+		}
+		else {
+			for (int i = 0; i < institutos.length; ++i) {
+			    retorno.add(institutos[i]);
+			}
+		}
+		return retorno;
+	}
 }
