98513d0bda26c3d5eb17ca35a3fe2583bc9921aa
rcastro33 <rcastro@programmer.net>
2020-11-14 00:32:39 -0300

    AltaCategoriaTest y pom incluyendo DBUnit

--- Proyecto/src/main/java/tests/AltaCategoriaTest.java
+++ Proyecto/src/main/java/tests/AltaCategoriaTest.java
@@ -1,8 +1,56 @@
 package tests;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import excepciones.CategoriaRepetidaException;
+import interfaces.Fabrica;
+import interfaces.IControladorAltaCategoria;
+import logica.Categoria;
+import logica.ManejadorCategoria;
+
 import static org.junit.Assert.*;
 
-public class AltaCategoriaTest {
+import javax.transaction.Transactional;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+
+public class AltaCategoriaTest {
+	static Fabrica fabrica;
+	static IControladorAltaCategoria icon; 
+	String nombrec1;
+	Integer id1 = 0;
+	String nombrec2;
+	Integer id2 = 0;
+	Categoria categoria1;
+	Categoria categoria2;
+	static ManejadorCategoria mC;
+	
+	@BeforeClass
+	public static void preparacionTests() {
+		fabrica = Fabrica.getInstancia();
+		icon = fabrica.getIControladorAltaCategoria();
+		mC = ManejadorCategoria.getInstancia();
+	}
+	
+	@Before
+	public void inicializarTest() {
+		nombrec1 = "jedi_arts" + this.id1.toString();
+		this.id1 += 1;
+		nombrec2 = "sith_arts" + this.id2.toString();
+		this.id2 += 1;
+	}
+	
+	@Test
+	public void categoriasConMismosDatos() throws CategoriaRepetidaException {
+		icon.darAltaCategoria(nombrec1);
+		icon.darAltaCategoria(nombrec2);
+		categoria1 = mC.find(nombrec1);
+		categoria2 = mC.find(nombrec2);
+		assertNotSame(categoria1,categoria2);
+	}
 }
