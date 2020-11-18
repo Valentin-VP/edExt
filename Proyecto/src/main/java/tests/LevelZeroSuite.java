225dda37e4bbcb5e5eb1462c5c21655b8da4b607
Valentin-VP <valevp2010@hotmail.com>
2020-11-18 11:03:04 -0300

    se agrega funciones del consultaCurso a levelzerosuite

--- Proyecto/src/main/java/tests/LevelZeroSuite.java
+++ Proyecto/src/main/java/tests/LevelZeroSuite.java
@@ -6,22 +6,36 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import datatypes.DtInstituto;
+import excepciones.SinCategorias;
 import excepciones.SinInstitutos;
 import interfaces.Fabrica;
 import interfaces.IControladorAltaUsuario;
+import interfaces.IControladorConsultaCurso;
 
 public class LevelZeroSuite {
 	static Fabrica fabrica;
 	static IControladorAltaUsuario icon;
+	static IControladorConsultaCurso iconConsultaCurso;
 	
 	@BeforeClass
 	public static void preparacionTests() {
 		fabrica = Fabrica.getInstancia();
 		icon = fabrica.getIControladorAltaUsuario();
+		iconConsultaCurso = fabrica.getIControladorConsultaCurso();
 	}
 	
 	@Test(expected = SinInstitutos.class)
 	public void test1_NoHayInstitutosParaListar() throws SinInstitutos {
 			ArrayList<DtInstituto> NoHayInstitutos = icon.listarInstitutos();
     }
+	
+	@Test
+	public void test2_listarInstitutosInfoCursoSinInstitutos() throws SinInstitutos {
+		iconConsultaCurso.listarInstitutos();
+	}
+	
+	@Test
+	public void test2_listarCategoriasInfoCursoSinInstitutos() throws SinCategorias {
+		iconConsultaCurso.listarCategorias();
+	}
 }
